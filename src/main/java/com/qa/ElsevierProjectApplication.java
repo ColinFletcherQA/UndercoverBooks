efb9192327463487c52a02e0e012be592e7f1558
jhg023@bucknell.edu <3a$r+@bY9V-A%sn$>
2018-06-26 14:51:14 -0400

    Required for AWS.

--- src/main/java/com/qa/ElsevierProjectApplication.java
+++ src/main/java/com/qa/ElsevierProjectApplication.java
@@ -2,12 +2,19 @@ package com.qa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.qa.controllers", "com.qa.restful.controllers", "com.qa.models", "com.qa.services"})
-public class ElsevierProjectApplication {
-
+public class ElsevierProjectApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ElsevierProjectApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ElsevierProjectApplication.class, args);
 	}
