b375fa087bdb1694490b61348450c61b0ce406be
mcginnjt <j.mcginnis@elsevier.com>
2018-06-28 16:43:18 -0400

    Fix validator for shipping

--- src/main/resources/static/js/validator.js
+++ src/main/resources/static/js/validator.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
                 if(input.css('display') != 'none'){
                     if(inputType == 'text' || inputType == 'email' || inputType == 'password'){
 
-                        if(input.attr('name') != 'addressLine2'){
+                        if(input.attr('name') != 'addressLine2' || input.attr('name') != 'addressLine21'){
                             if(!input.val()){
                                 input.css('border', '3px solid red');
                                 valid = false;
@@ -21,7 +21,7 @@ $(document).ready(function() {
                                 input.css('border-color', '');
                             }
                         }
-                    } else if (inputType == 'tel' && input.attr('name') != "cardNumber" && input.attr("name") != "postcode"){
+                    } else if (inputType == 'tel' && input.attr('name') != "cardNumber" && input.attr("name") != "postcode" && input.attr("name") != "postcode1"){
                         if(input.val().length != 10){
                             input.css('border', '3px solid red');
                             valid = false;
